d8e7edc45b62f6e3d3143972a753db2e573ca483
robjfoster <r.j.foster@sheffield.ac.uk>
2025-05-04 14:05:32 +0100

    Add batching

--- tools/macro_gen.py
+++ tools/macro_gen.py
@@ -52,6 +52,14 @@ def parse_arguments():
         help="The height of the tube (float)."
     )
     
+    parser.add_argument(
+        '--nBatches',
+        type=int,
+        required=False,
+        default=1,
+        help="The number of batches to run (integer)."
+    )
+    
     args = parser.parse_args()
     return args
 
@@ -64,25 +72,26 @@ if __name__ == "__main__":
     generated_outputfiles = []
     for rock_material in args.rockMaterials:
         for snow_depth in args.snowDepths:
-            # Create file and open it for writing
-            filename = f"./{args.projectName}/run_{rock_material}_rockmaterial_{snow_depth}m_snowdepth_{args.nEvents}evts.mac"
-            generated_macros.append(filename)
-            outputfile = f"run_{rock_material}_rockmaterial_{snow_depth}m_snowdepth_{args.nEvents}evts.csv"
-            generated_outputfiles.append(outputfile)
-            Path(f"./{args.projectName}").mkdir(parents=True, exist_ok=True)
-            with open(filename, 'w') as file:
-                # Write the macro commands to the file
-                file.write(f"/run/initialize\n")
-                file.write(f"/am/outputfile {outputfile}\n")
-                if args.embedTube:
-                    file.write(f"/snowsim/det/embedTube true\n")
-                file.write(f"/snowsim/det/setRockMaterial {rock_material}\n")
-                file.write(f"/snowsim/det/setRockThickness 2 m\n")
-                file.write(f"/snowsim/det/setSnowDepth {snow_depth} m\n")
-                if args.embedTube:
-                    file.write(f"/snowsim/det/setTubeOffsetHeight {args.tubeHeight} m\n")
-                file.write(f"/control/execute crysetup.mac\n")
-                file.write(f"/run/beamOn {args.nEvents}\n")
+            for batch in range(args.nBatches):
+                # Create file and open it for writing
+                filename = f"./{args.projectName}/run_{rock_material}_rockmaterial_{snow_depth}m_snowdepth_{args.nEvents}evts_batch{batch}.mac"
+                generated_macros.append(filename)
+                outputfile = f"run_{rock_material}_rockmaterial_{snow_depth}m_snowdepth_{args.nEvents}evts_batch{batch}.csv"
+                generated_outputfiles.append(outputfile)
+                Path(f"./{args.projectName}").mkdir(parents=True, exist_ok=True)
+                with open(filename, 'w') as file:
+                    # Write the macro commands to the file
+                    file.write(f"/run/initialize\n")
+                    file.write(f"/am/outputfile {outputfile}\n")
+                    if args.embedTube:
+                        file.write(f"/snowsim/det/embedTube true\n")
+                    file.write(f"/snowsim/det/setRockMaterial {rock_material}\n")
+                    file.write(f"/snowsim/det/setRockThickness 2 m\n")
+                    file.write(f"/snowsim/det/setSnowDepth {snow_depth} m\n")
+                    if args.embedTube:
+                        file.write(f"/snowsim/det/setTubeOffsetHeight {args.tubeHeight} m\n")
+                    file.write(f"/control/execute crysetup.mac\n")
+                    file.write(f"/run/beamOn {args.nEvents}\n")
                 
     # Generate task spooler script
     Path(f"../results/{args.projectName}").mkdir(parents=True, exist_ok=True)
